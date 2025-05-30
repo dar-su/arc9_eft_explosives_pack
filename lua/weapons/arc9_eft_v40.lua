23f3d4715dd47b421431f47c88276ab25018125e
Darsu <95997959+dar-su@users.noreply.github.com>
2025-05-30 22:13:09 +0700

    f1 reduced delay

--- lua/weapons/arc9_eft_v40.lua
+++ lua/weapons/arc9_eft_v40.lua
@@ -4,10 +4,10 @@ SWEP.SubCategory = ARC9:GetPhrase("eft_subcat_grenades")
 SWEP.Spawnable = true
 SWEP.AdminOnly = false
 
-SWEP.PrintName = ARC9:GetPhrase("eft_weapon_v40_full")
-SWEP.ShortPrintName = "eft_weapon_v40"
+SWEP.PrintName = ARC9:GetPhrase("eft_weapon_v40_full_alt")
+SWEP.ShortPrintName = ARC9:GetPhrase("eft_weapon_v40_alt")
 SWEP.Class = ARC9:GetPhrase("eft_class_weapon_grenade")
-SWEP.Description = ARC9:GetPhrase("eft_weapon_v40_desc")
+SWEP.Description = ARC9:GetPhrase("eft_weapon_v40_desc_alt")
 
 SWEP.ViewModel = "models/weapons/arc9/darsu_eft/c_v40.mdl"
 SWEP.WorldModel = "models/weapons/arc9/darsu_eft/w_v40_unthrowed.mdl"
