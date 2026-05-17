e854f1d089da9375a1d74500a89add699608e4b3
Darsu <95997959+dar-su@users.noreply.github.com>
2026-05-17 16:49:03 +0700

    Update arc9_eft_fn40gl.lua

--- lua/weapons/arc9_eft_fn40gl.lua
+++ lua/weapons/arc9_eft_fn40gl.lua
@@ -183,7 +183,7 @@ SWEP.DropMagazinePos = Vector(0, 0, 0)
 SWEP.DropMagazineAng = Angle(90, 180, 90)
 SWEP.DropMagazineVelocity = Vector(0, 40, 20)
 SWEP.DropMagazineSounds = ARC9EFT.ShellsHeavy
-SWEP.DropMagazine = "arc9_eft_magdropeffect"
+SWEP.DropMagazineEffect = "arc9_eft_magdropeffect"
 
 ------------------------- |||           Animations            ||| -------------------------
 
